57637a2824ed9a08dcac89c55bccb82d9d6e3057
aoroudjev <a.oroudjev@gmail.com>
2023-12-04 23:37:52 -0800

    Add main, game_valid, draw_valid

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,3 +1,41 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sum: usize = 0;
+    let input: String = fs::read_to_string("./src/inputs/day02.txt").unwrap();
+    let lookup: HashMap<&str, u32> =
+        HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+
+    for (index, line) in input.lines().enumerate() {
+        let game: Vec<&str> = line.split(':').nth(1).unwrap().split(';').collect();
+
+        if game_valid(&game, &lookup) {
+            sum += index + 1;
+        }
+    }
+
+    println!("{}", sum);
+}
+
+fn game_valid(game: &Vec<&str>, lookup: &HashMap<&str, u32>) -> bool {
+    for draw_set in game {
+        if !draw_valid(draw_set, lookup) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn draw_valid(draw: &str, lookup: &HashMap<&str, u32>) -> bool {
+    for color in draw.split(',') {
+        let (num, color) = color.split_ascii_whitespace().collect_tuple().unwrap();
+        if num.parse::<u32>().unwrap() > *lookup.get(color).unwrap() {
+            return false;
+        }
+    }
+
+    true
 }
